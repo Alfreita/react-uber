7aef7bd3e8af2b347d845c75920381ac479c12ce
Victor Almeida <victor.freitas08@hotmail.com>
2019-03-14 20:45:01 -0300

    maps

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import MapView from 'react-native-maps';
+import {View} from 'react-native';
+
+const App = () =>( 
+<View style={{flex:1}}>
+<MapView
+    style={{flex:1}}
+    reggion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+    }}  
+        showUserLocation
+        loadingEnabled
+    />
+</View>
+ );
+
+export default App;
